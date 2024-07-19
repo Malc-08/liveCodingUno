4c047bb5605b73e5f13609924f5461145b5b3a68
Mariana Lopez <marianaalexa76@gmail.com>
2024-07-19 11:36:13 -0600

    modify: realice cambios en app.js e index.html para el ejercicio de live coding

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // Escribe tu código aquí.
+
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+console.log(myArray.map(('bocina', myArray => i = 0, i >= myArray)));
+
